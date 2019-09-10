db329c0cdf8890b2a6af665bb3f351bdc465da97
pengkuang <31425596+PengKuang@users.noreply.github.com>
2019-09-11 09:18:47 +1000

    update sql file for student/assessment/
    added PKs and FKs

--- rf_student_assessment_selectedComment.sql
+++ rf_student_assessment_selectedComment.sql
@@ -4,27 +4,34 @@ create database test;
 use test;
 
 create table student (
-	idStudent int,
-    firstName VARCHAR(50),
+	idStudent int NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
     middleName VARCHAR(50),
-    lastName VARCHAR(50),
-    studentNumber VARCHAR(10),
-    studentEmail VARCHAR(200)
+    lastName VARCHAR(50) NOT NULL,
+    studentNumber VARCHAR(10) NOT NULL,
+    studentEmail VARCHAR(200) NOT NULL,
+    PRIMARY KEY (idStudent)
 );
 
 create table assessment (
-	idCriteria int,
-    idProject int,
-    idStudent int,
-    idMarker int,
-    score double
+	idCriteria int NOT NULL,
+    idProject int NOT NULL,
+    idStudent int NOT NULL,
+    idMarker int NOT NULL,
+    score double,
+    PRIMARY KEY (idCriteria, idProject, idStudent, idMarker),
+    FOREIGN KEY (idStudent) REFERENCES student (idStudent)
+    /*FOREIGN KEY (idMarker) REFERENCES marker (id),
+    FOREIGN KEY (idProject, idCriteria) REFERENCES ProjectSetCriteria (idProject, idCriteria)*/
 );
 
 create table selectedComment (
-	idCriteria int,
-    idProject int,
-    idStudent int,
-    idMarker int,
-    idExpandedComment int
+	idCriteria int NOT NULL,
+    idProject int NOT NULL,
+    idStudent int NOT NULL,
+    idMarker int NOT NULL,
+    idExpandedComment int NOT NULL,
+    PRIMARY KEY (idCriteria, idProject, idStudent, idMarker, idExpandedComment),
+    FOREIGN KEY (idCriteria, idProject, idStudent, idMarker) REFERENCES assessment (idCriteria, idProject, idStudent, idMarker)
 );
 
